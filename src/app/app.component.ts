cd2689efb15518615700b920182c2bb28c69eb33
MytilinisV <vasilismitilinis@gmail.com>
2024-03-28 20:41:35 +0200

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Vasilis';
+
+  person = {
+    givenName: 'Bill',
+    sirName: 'Mytilinis',
+    age: 0x28,
+    email: 'vasilismitilinis@gmail.com'
+  }
 }
